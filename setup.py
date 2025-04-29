8633ff3424cf121982ceb220463416cdc4a93f54
shailendrabhandari <shailendra.vandari@gmail.com>
2025-04-29 15:38:15 +0200

    docs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dueling-bandit",
-    version="0.1.0",
+    version="0.1.1",
     packages=["dueling_bandit"],  
     package_dir={"dueling_bandit": "dueling_bandit"},
     include_package_data=True,
